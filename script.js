9fd8f9e4ce2588687f649fdbc682ffe5f38ceea1
RAKESH M B <118419906+RakeshCherry@users.noreply.github.com>
2023-07-31 18:17:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-<title>Volume of a Sphere</title>
-<style> 
-body{padding-top:30px;}
-label,input{display:block;} 
-</style>
-</head>
-<body>
-	<p>Input radius value and get the volume of a sphere.</p>
-	<form action="" method="post" id="MyForm">
-	<label for="radius">Radius</label><input type="text" name="radius" id="radius" required>
-	<label for="volume">Volume</label><input type="text" name="volume" id="volume">
-	<input type="submit" value="Calculate" id="submit">    </form>
-	<script type="text/javascript" src="./script.js"></script>
-</body>
-</html>
- 
- 
- 
-// js 
- 
+
  function volume_sphere() {
             var radius = document.getElementById('radius').value;
             radius = Math.abs(radius);
